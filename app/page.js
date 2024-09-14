4dc337400fe7cc38b691ba2d908c7f5c4adc99db
talal748 <talal.habib@edu.sait.ca>
2024-09-13 18:10:01 -0600

    Final changes to week 2 assignment
    Added a text center element, and finished up the assignment

--- app/page.js
+++ app/page.js
@@ -4,8 +4,8 @@ import StudentInfo from "./week-2/student-info";
 export default function Home() {
   return (
     <main>
-      <h1 className="text-2xl">CPRG 306: Web Development 2 - Assignments</h1>
-      <Link href="./week-2" className="underline text-red-800 hover:text-red-600" >Week 2 Assignment Link</Link>
+      <h1 className="text-2xl text-center">CPRG 306: Web Development 2 - Assignments</h1>
+      <Link href="./week-2" className="underline text-red-800 hover:text-red-600">Week 2 Assignment Link</Link>
     </main>
   );
 }
