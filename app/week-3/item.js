83310e3a7b339f58c9900c5eeff6f9541320e5ee
talal748 <talal.habib@edu.sait.ca>
2024-09-26 09:18:05 -0600

    Added in a bit of styling
    Added styling to the Shopping List header and made the div statement an inline-block style and gave it padding of 5 pixels

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -1,6 +1,6 @@
 export default function Item ({name, quantity, category}) {
     return (
-        <div>
+        <div className="inline-block pxy-5">
             <li>{name}</li>
             <li>Quantity: {quantity}</li>
             <li>Category: {category}</li>
--- app/week-3/page.js
+++ app/week-3/page.js
@@ -2,7 +2,7 @@ import ItemList from "./item-list";
 
 export default function Page() {
     <main>
-        <h1>Shopping List:</h1>
+        <h1 className="font-serif text-blue-500">Shopping List:</h1>
         <ItemList/>
     </main>
 }
